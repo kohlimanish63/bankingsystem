b1dbc2e53c7cd208e84d645bef2cd705f8e07b2c
Manish <kohlimanish63@gmail.com>
2021-01-21 19:10:49 +0530

    add git gnore port files

--- index.js
+++ index.js
@@ -78,6 +78,11 @@ app.get("/customers/:customerId", (req, res) => {
     })
 })
 
-app.listen(process.env.PORT || 3000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
     console.log("server is running on port 3000");
 })
